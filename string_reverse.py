6309ccad60b854b65dee0e4b844e4cb907fe612c
MarcinPierchala <macintecmarcin@gmail.com>
2021-09-03 19:48:13 +0200

    3rd_version

--- string_reverse.py
+++ string_reverse.py
@@ -1,5 +1,10 @@
 podaj = input("podaj text do odwrócenia:  ")
 dlug = len(podaj)
+new = ''
 for x, z in enumerate(podaj):
     print(podaj[dlug-x-1])
+    new += podaj[dlug-x-1]
+
+print(new)
+
     
